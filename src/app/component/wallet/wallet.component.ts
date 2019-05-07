0ff2a6092153f860c04702b9e9b8beef3cbda8dc
Szy Boglárka <szy.bogi@gmail.com>
2019-05-07 10:51:59 +0200

    delete category interface

--- src/app/component/wallet/wallet.component.ts
+++ src/app/component/wallet/wallet.component.ts
@@ -1,12 +1,10 @@
-import { FormBuilder, FormControl } from '@angular/forms';
 import { WalletPageComponent } from './../page/wallet-page/wallet-page.component';
-import { WalletFormComponent } from './../wallet-form/wallet-form.component';
-import { Wallet, walletSchema } from '../../model/wallet.interface';
+import { Wallet } from '../../model/wallet.interface';
 import { Component, OnInit, Input } from '@angular/core';
 import { DatabaseService } from 'src/app/service/database.service';
-import { withLatestFrom } from 'rxjs/internal/operators/withLatestFrom';
-import { Observable } from 'rxjs';
+import { Observable, combineLatest } from 'rxjs';
 import { RxDocument } from 'rxdb';
+import { Transaction } from 'src/app/model/transaction.class';
 
 @Component({
 	selector: 'app-wallet',
@@ -14,7 +12,12 @@ import { RxDocument } from 'rxdb';
 	styleUrls: ['./wallet.component.scss']
 })
 export class WalletComponent implements OnInit {
-	constructor(private databaseService: DatabaseService, private walletPageComponent: WalletPageComponent) {}
+	public transactionsReplayed$: Observable<RxDocument<Transaction>[]>;
+	public walletsReplayed$: Observable<RxDocument<Wallet>[]>;
+	constructor(private databaseService: DatabaseService, private walletPageComponent: WalletPageComponent) {
+		this.transactionsReplayed$ = databaseService.transactionsReplayed$;
+		this.walletsReplayed$ = databaseService.walletsReplayed$;
+	}
 	@Input()
 	public wallet: RxDocument<Wallet>;
 
